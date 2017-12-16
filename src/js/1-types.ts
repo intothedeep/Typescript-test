d731c942b1f43941a2613396268c48b5c0bede98
intothedeep <victor.taek.lim@gmail.com>
2017-12-16 22:33:28 +0900

    typescript practice

---
+++ src/js/1-types.ts
@@ -0,0 +1,35 @@
+// typescript
+let str : string;
+let num : number;
+let boolean : boolean;
+
+let firstName : string = "victor";
+let age : number = 15;
+let nameStr : string = `
+  ex1) types
+  My name is ${ firstName }!
+
+  Nice to Meet you!. I am ${ age + 1 } years old!
+`;
+
+document.getElementById('ts').innerHTML = nameStr;
+
+
+// enum
+enum Color {
+  r,
+  g,
+  b
+}
+let c : Color = Color.r;
+console.log(c);
+let cs : string = Color[2];
+console.log(cs);
+
+// for (let x in Color) console.log(x);
+console.log(Color);
+
+// Assertion (casting)
+let someValue: any = "this is a string";
+let strLength: number = (<string>someValue).length;
+// let strLength: number = (someValue as string).length;
---
+++ src/js/2-declarations.ts
@@ -0,0 +1,121 @@
+// declarations var vs. let, const
+
+// function f(shouldInitialize: boolean) {
+//     if (shouldInitialize) {
+//         var x = 10;
+//     }
+//
+//     return x;
+// }
+//
+// f(true);  // returns '10'
+// f(false); // returns 'undefined'
+// var can be accesed
+// var declarations are accessible anywhere within their containing function, module, namespace, or global scope - all which we’ll go over later on - regardless of the containing block.
+
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(
+//       function() {
+//         console.log("1st " + i);
+//       },
+//       100 * i
+//     );
+// }
+//
+// // Every function expression we pass to setTimeout actually refers to the same i from the same scope.
+// // Let’s take a minute to consider what that means. setTimeout will run a function after some number of milliseconds, but only after the for loop has stopped executing; By the time the for loop has stopped executing, the value of i is 10. So each time the given function gets called, it will print out 10!
+//
+// // A common work around is to use an IIFE - an Immediately Invoked Function Expression - to capture i at each iteration:
+// for (var i = 0; i < 10; i++) {
+//     // capture the current state of 'i'
+//     // by invoking a function with its current value
+//     (
+//       function(i) {
+//           setTimeout(function() { console.log("2nd " + i); }, 100 * i);
+//       }
+//     )
+//     (
+//       i
+//     );
+// }
+// // This odd-looking pattern is actually pretty common. The i in the parameter list actually shadows the i declared in the for loop, but since we named them the same, we didn’t have to modify the loop body too much.
+//
+//
+// /*
+// *block-scoping
+// */
+//
+//
+// for (let i = 0; i < 10 ; i++) {
+//     setTimeout(function() { console.log(i); }, 100 * i);
+// }
+
+
+// ------------------------- const
+
+// const numLivesForCat = 9;
+// const kitty = {
+//     name: "Aurora",
+//     numLives: numLivesForCat,
+// }
+//
+// // Error
+// kitty = {
+//     name: "Danielle",
+//     numLives: numLivesForCat
+// };
+//
+// // all "okay"
+// kitty.name = "Rory";
+// kitty.name = "Kitty";
+// kitty.name = "Cat";
+// kitty.numLives--;
+
+
+// ----------Destructuring
+let a : Array = [1, 2, 3];
+let [x, y] = a;
+console.log(x);
+console.log(y);
+console.log("-------");
+
+[x, y] = [y, x];
+console.log(x);
+console.log(y);
+console.log("-------");
+
+let [x, ...y] = a;
+console.log(x);
+console.log(y);
+console.log("-------");
+
+let [x:xx = 99, y:yy] = a;
+console.log(xx);
+console.log(yy);
+console.log(y);
+console.log("-------");
+
+// ? => optional!!!!!
+function keepWholeObject(wholeObject: { a: string, b?: number }) {
+    let { a, b = 1001 } = wholeObject;
+}
+
+//  ------------spread
+let first = [1, 2];
+let second = [3, 4];
+let bothPlus = [0, ...first, ...second, 5];
+
+let defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
+let search = { food: "rich", ...defaults };
+// Then the food property in defaults overwrites food: "rich", which is not what we want in this case.
+
+// Object spread also has a couple of other surprising limits. First, it only includes an objects’ own, enumerable properties. Basically, that means you lose methods when you spread instances of an object:
+class C {
+  p = 12;
+  m() {
+  }
+}
+let c = new C();
+let clone = { ...c };
+clone.p; // ok
+// clone.m(); // error!
